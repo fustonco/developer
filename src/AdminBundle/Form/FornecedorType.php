4b9e0cfc4dd1cff59e8c9cbfa7f500c4cb874490
Pedro Henrique Lopes <pedro.lopes@fuston.com.br>
2018-01-29 13:24:08 -0200

    - criado CRUD Fornecedor no AdminBundle.

---
+++ src/AdminBundle/Form/FornecedorType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FornecedorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nome')
+            ->add('cnpj')
+            ->add('cpf')
+            ->add('telefone')
+            ->add('endereco')
+            ->add('ativo')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AdminBundle\Entity\Fornecedor'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'adminbundle_fornecedor';
+    }
+}
