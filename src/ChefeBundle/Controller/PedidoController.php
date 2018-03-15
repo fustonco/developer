5f5e6aa10b58abfa63ea3f9f4340f9c1760481df
Lennon Milicic Costa <lennon@fuston.com.br>
2018-03-15 17:54:34 -0300

    Correcao

--- src/ChefeBundle/Controller/PedidoController.php
+++ src/ChefeBundle/Controller/PedidoController.php
@@ -54,7 +54,6 @@ class PedidoController extends Controller
         );
     }
 
-
     /**
      * Lists all Pedido entities.
      *
@@ -74,6 +73,38 @@ class PedidoController extends Controller
         INNER JOIN historico h ON ht.id = h.id AND h.idPara = :para
         INNER JOIN funcionario f ON f.id = h.idPara
         INNER JOIN tipo_usuario tu ON tu.id = f.idTipo
+        WHERE p.status != 3
+        ORDER BY p.id DESC;
+        ");
+        $entities->bindValue("para", $this->getUser()->getId());
+        $entities->execute();
+        $entities = $entities->fetchAll();
+
+        return array(
+            'entities' => $entities
+        );
+    }
+
+    /**
+     * Lists all Pedido entities.
+     *
+     * @Route("/recusado", name="chefe_pedido_recusado")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexRecusadoAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getConnection()->prepare("
+        SELECT f.nome funcionario, tu.id id_tipo_funcionario, tu.nome tipo_funcionario, p.id, p.codigo, p.idTipo, p.idFornecedor, p.data_pedido dataPedido, p.valor, p.descricao, p.ativo, sp.nome status
+        FROM pedido p
+        INNER JOIN status_pedido sp ON sp.id = p.status
+        INNER JOIN (SELECT MAX(id) id, idPedido FROM historico GROUP BY idPedido) ht ON ht.idPedido = p.id
+        INNER JOIN historico h ON ht.id = h.id AND h.idPara = :para
+        INNER JOIN funcionario f ON f.id = h.idPara
+        INNER JOIN tipo_usuario tu ON tu.id = f.idTipo
+        WHERE p.status = 3
         ORDER BY p.id DESC;
         ");
         $entities->bindValue("para", $this->getUser()->getId());
