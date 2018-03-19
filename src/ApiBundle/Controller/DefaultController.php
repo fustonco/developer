2ede7381d03eb80550da1c6e02d599146f5aebb5
Lennon Milicic Costa <lennon@fuston.com.br>
2018-03-19 14:02:46 -0300

    API

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -2,18 +2,36 @@
 
 namespace ApiBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/hello/{name}")
-     * @Template()
-     */
-    public function indexAction($name)
+    public function getContent($request)
     {
-        return array('name' => $name);
+        $data = json_decode($request->getContent(), true);
+        return !empty($data) ? $data : array();
+    }
+
+    /**
+     * @Route("/v1/login")
+     * @Method("POST")
+     */
+    public function v1LoginAction(Request $request)
+    {
+        $request->request->replace($this->getContent($request));
+
+        try {
+            return new Response(json_encode([
+                'user' => $request->get('username'),
+                'pass' => $request->get('password')
+            ]), 200);
+        } catch(\Exception $e) {
+            return new Response(json_encode('errorroror'), 500);
+        }
     }
 }
