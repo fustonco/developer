f767c9a8d91731180e6ee34c509be6b84921ffe2
Lennon Milicic Costa <lennon@fuston.com.br>
2018-03-20 15:02:24 -0300

    LOGIN

--- src/LoginBundle/Controller/DefaultController.php
+++ src/LoginBundle/Controller/DefaultController.php
@@ -2,9 +2,18 @@
 
 namespace LoginBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Security\Http\Firewall\ListenerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Authentication\AuthenticationProviderManager;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
 
 class DefaultController extends Controller
 {
@@ -14,15 +23,30 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $authUtils = $this->get("security.authentication_utils");
-        
+
         $error = $authUtils->getLastAuthenticationError();
 
         $lastUsername = $authUtils->getLastUsername();
     
         return $this->render("LoginBundle:Default:index.html.twig", array(
             "last_username" => $lastUsername,
-            "error" => $error
+            "error" => $error,
         ));
+    }
+
+    /**
+     * @Route("/logar/{_username}/{_password}" , name="loginReact")
+     */
+    public function indexLogarAction($_username, $_password)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userEntity = $em->getRepository('LoginBundle:Funcionario')->findOneBy([
+            'email' => $_username,
+            'senha' => md5($_password)
+        ]);
+        $token = new UsernamePasswordToken($userEntity, null, 'login_secured_area', $userEntity->getRoles());
+        $this->get('security.context')->setToken($token);
+        return $this->redirect("/redirectlogin");
     }
     
      /**
