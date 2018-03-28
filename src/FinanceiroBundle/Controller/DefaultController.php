5a2a249a670cde0c246d7678702612febefbba92
ion-project <gabrielcursino.medeiros@hotmail.com>
2018-03-28 15:20:59 -0300

    Inicio do financeiro

--- src/FinanceiroBundle/Controller/DefaultController.php
+++ src/FinanceiroBundle/Controller/DefaultController.php
@@ -15,18 +15,23 @@ class DefaultController extends Controller
         
         $em = $this->getDoctrine()->getManager();
         
-        $parcelas = $em->getRepository("FinanceiroBundle:Parcelas")->findOneBy(array(
-            'dataVencimento' => date_create(),
-            'status' => '1'
-        ));
-
-        //$idPag = $parcelas->getIdPagamento();
-        
-        
-        //findBy(array('data_vencimento' <= 'current_date', 'status' => '1'))
+        $pedidos = $em->getConnection()->prepare("
+        SELECT f.nome funcionario, tu.id id_tipo_funcionario, tu.nome tipo_funcionario, p.id, p.codigo, p.idTipo, p.idFornecedor, p.data_pedido dataPedido, p.valor, p.descricao, p.ativo, sp.nome status
+        FROM pedido p
+        INNER JOIN status_pedido sp ON sp.id = p.status
+        INNER JOIN (SELECT MAX(id) id, idPedido FROM historico GROUP BY idPedido) ht ON ht.idPedido = p.id
+        INNER JOIN historico h ON ht.id = h.id AND h.idPara = :para
+        INNER JOIN funcionario f ON f.id = h.idPara
+        INNER JOIN tipo_usuario tu ON tu.id = f.idTipo
+        WHERE p.status != 3
+        ORDER BY p.id DESC;
+        ");
+        $pedidos->bindValue("para", $this->getUser()->getId());
+        $pedidos->execute();
+        $pedidos = $pedidos->fetchAll();
         
         return $this->render("FinanceiroBundle:Default:index.html.twig", [
-            'parcelas'  => $parcelas
+            'pedidos'  => $pedidos
         ]);
     }
 }
